cb1ee442ffe28673aac781f51f52c87cc914fe7b
NabilaErwintria <nzaahiya@gmail.com>
2024-09-15 18:12:33 +0700

    update

--- resources/views/home/profileMahasiswa.blade.php
+++ resources/views/home/profileMahasiswa.blade.php
@@ -7,51 +7,17 @@
 </head>
 <body>
     <div class="v42_461">
-        <div class="name"></div>
+        <div class="v42_462"></div>
         <div class="v42_464">
             <div class="v42_465">
                 <div class="name"></div>
                 <div class="name"></div>
-                <span class="v42_468">Eunike Alfrita</span>
+                <span class="v42_468">Eunike Alfrita MW</span>
                 <span class="v42_469">187345678</span>
             </div>
         </div>
         <div class="v42_470">
-            <span class="v42_471">Monitoring PKL</span>
-            <div class="v42_472">
-                <div class="v44_413">
-                    <div class="v42_473">
-                        <span class="v42_474">Profil</span>
-                        <div class="name"></div>
-                    </div>
-                </div>
-                <div class="v44_415">
-                    <div class="v42_483">
-                        <div class="name"></div>
-                        <span class="v42_485">Informasi PKL</span>
-                        <div class="name"></div>    
-                    </div>
-                </div>
-                <div class="v44_416">
-                    <div class="v42_487">
-                        <span class="v42_488">Monitoring PKL</span>
-                        <div class="name"></div>
-                    </div>
-                </div>
-                <div class="v44_417">
-                    <div class="v42_493">
-                        <div class="name"></div>
-                        <span class="v42_495">Laporan</span>
-                        <div class="name"></div>
-                    </div>
-                </div>
-            </div>
-            <div class="v43_587">
-                <div class="name"></div>
-                <span class="v43_589">Seminar</span>
-                <div class="name"></div>
-                <div class="name"></div>
-            </div>
+            <span class="v42_471">Profil</span>
             <div class="v44_414">
                 <div class="v42_535">
                     <div class="name"></div>
@@ -59,10 +25,46 @@
                     <div class="v43_585"></div>
                 </div>
             </div>
-            <div class="v44_419">
-                <div class="v42_476">
-                    <span class="v42_477">Download</span>
+            <div class="v49_1136">
+                <div class="v42_472">
+                    <div class="v44_413">
+                        <div class="v42_473">
+                            <span class="v42_474"><a href="profileMahasiswa">Profil</a></span>
+                            <div class="name"></div>
+                        </div>
+                    </div>
+                    <div class="v44_415">
+                        <div class="v42_483">
+                            <div class="name"></div>
+                            <span class="v42_485">Informasi PKL</span>
+                            <div class="name"></div>
+                        </div>
+                    </div>
+                    <div class="v44_416">
+                        <div class="v42_487">
+                            <span class="v42_488">Monitoring PKL</span>
+                            <div class="name"></div>
+                        </div>
+                    </div>
+                    <div class="v44_417">
+                        <div class="v42_493">
+                            <div class="name"></div>
+                            <span class="v42_495">Laporan</span>
+                            <div class="name"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="v43_587">
                     <div class="name"></div>
+                    <span class="v43_589">Seminar</span>
+                    <div class="name"></div>
+                    <div class="name"></div>
+                </div>
+                <div class="v44_419">
+                    <div class="v42_476">
+                        <span class="v42_477">Download</span>
+                        <div class="name"></div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -99,6 +101,34 @@
                 <div class="v44_454"></div>
             </div>
         </div>
+        <div class="v53_1140">
+            <div class="v53_1141"></div>
+            <div class="v53_1142">
+                <span class="v53_1143">Nama Lengkap</span>
+                <div class="v53_1144"></div>
+            </div>
+            <div class="v53_1145">
+                <span class="v53_1146">NIM</span>
+                <div class="v53_1147"></div>
+            </div>
+            <div class="v53_1148">
+                <span class="v53_1149">Program Studi</span>
+                <div class="v53_1150"></div>
+            </div>
+            <div class="v53_1151">
+                <span class="v53_1152">Email</span>
+                <div class="v53_1153"></div>
+            </div>
+            <div class="v53_1154">
+                <span class="v53_1155">Nomor HP</span>
+                <div class="v53_1156"></div>
+            </div>
+            <div class="v53_1157">
+                <span class="v53_1158">Dosen Wali</span>
+                <div class="v53_1159"></div>
+            </div>
+        </div>
+        <span class="v61_712">Profil</span>
     </div>
 </body>
 </html>
