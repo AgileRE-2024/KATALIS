a542e97d6d10b10322e273ba7980f6ff0043fa3b
NabilaErwintria <nzaahiya@gmail.com>
2024-09-15 18:54:15 +0700

    update

--- resources/views/home/logbook.blade.php
+++ resources/views/home/logbook.blade.php
@@ -1,96 +1,91 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Monitoring PKL</title>
     <link href="https://fonts.googleapis.com/css?family=Inter&display=swap" rel="stylesheet" />
     <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet" />
-    <link href="{{ asset('assets/css/main.css') }}" rel="stylesheet" />
+    <link href="./assets/css/logbook.css" rel="stylesheet" />
+    <title>Document</title>
 </head>
-
 <body>
-    <div class="v113_1089">
+    <div class="v43_816">
+        <div class="v43_824">
+            <span class="v43_825">Monitoring PKL</span>
+            <div class="v43_826">
+                <div class="v43_827">
+                    <div class="v43_828">
+                        <span class="v43_829">Profil</span>
+                        <div class="name"></div>
+                    </div>
+                </div>
+                <div class="v43_831">
+                    <div class="v43_832">
+                        <div class="name"></div>
+                        <span class="v43_834">Informasi PKL</span>
+                        <div class="name"></div>
+                    </div>
+                </div>
+                <div class="v43_836">
+                    <div class="v43_837">
+                        <span class="v43_838">Monitoring PKL</span>
+                        <div class="name"></div>
+                    </div>
+                </div>
+                <div class="v43_840">
+                    <div class="v43_841">
+                        <div class="name"></div>
+                        <span class="v43_843">Laporan</span>
+                        <div class="name"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="v43_845">
+                <div class="name"></div>
+                <span class="v43_847">Seminar</span>
+                <div class="name"></div>
+                <div class="name"></div>
+            </div>
+            <div class="v43_850">
+                <div class="v43_851">
+                    <span class="v43_852">Download</span>
+                    <div class="name"></div>
+                </div>
+            </div>
+            <div class="v43_854">
+                <div class="v43_855">
+                    <div class="name"></div>
+                    <span class="v43_857">Dosen Pembimbing</span>
+                    <div class="v43_858"></div>
+                </div>
+            </div>
+        </div>
+        <div class="v43_819">
+            <div class="name"></div>
+            <div class="name"></div>
+            <span class="v43_822">Eunike Alfrita MW</span>
+            <span class="v43_823">187345678</span>
+        </div>
+        <div class="v80_826">
+            <div class="name"></div>
+            <div class="name"></div>
+            <span class="v80_829">Eunike Alfrita MW</span>
+            <span class="v80_830">187345678</span>
+        </div>
+        <div class="v43_861">
+            <div class="name"></div>
+            <div class="name"></div>
+        </div>
+        <span class="v61_706">Monitoring PKL</span>
         <div class="name"></div>
-        <span class="v113_1116">Laporan</span>
-        <div class="v123_1694">
-            <div class="v123_1695">
-                <div class="name"></div>
-                <div class="name"></div>
-                <span class="v123_1698">Abi</span>
-                <span class="v123_1699">187345678</span>
-            </div>
+        <div class="v80_838">
+            <div class="name"></div>
+            <span class="v80_840">Tambah Logbook</span>
         </div>
-        <div class="v113_1090">
-            <span class="v113_1091">Monitoring PKL</span>
-            <div class="v144_287">
-            <button onclick="location.href='{{ url('profil') }}'">
-                <div class="v113_1092">
-                    
-                    <span class="v113_1094">Profil</span>
-                    
-                    <div class="name"></div>
-                </div>
-            </button>
-            <button onclick="location.href='{{ url('download') }}'">
-                <div class="v113_1095">
-                    <span class="v113_1097">Download</span>
-                    <div class="name"></div>
-                </div>
-            </button>
-            <button onclick="location.href='{{ url('pengajuan') }}'">
-                <div class="v113_1098">
-                    <div class="name"></div>
-                    <span class="v113_1100">Pengajuan PKL</span>
-                    <div class="name"></div>
-                </div>
-            </button>
-            <button onclick="location.href='{{ url('informasi') }}'">
-                <div class="v113_1101">
-                    <div class="name"></div>
-                    <span class="v113_1103">Informasi PKL</span>
-                    <div class="name"></div>
-                </div>
-            </button>
-            <button onclick="location.href='{{ url('logbook') }}'">
-                <div class="v113_1104">
-                    <span class="v113_1106">Logbook</span>
-                    <div class="name"></div>
-                </div>
-            </button>
-            <button onclick="location.href='{{ url('bimbingan') }}'">
-                <div class="v113_1107">
-                    <span class="v113_1109">Bimbingan</span>
-                    <div class="name"></div>
-                </div>
-            </button>
-            <button onclick="location.href='{{ url('laporan') }}'">
-                <div class="v120_1310">
-                    <div class="name"></div>
-                    <span class="v120_1312">Laporan</span>
-                    <div class="name"></div>
-                </div>
-            </div>
-        </div>
-        <div class="v118_1244">
-            <div class="v113_1117">
-                <span class="v113_1118">Dokumen</span>
-            </div>
-            <div class="v113_1119">
-                <span class="v113_1120">Template Surat Pengantar PKL</span>
-            </div>
-            <div class="v113_1121">
-                <span class="v113_1122">Kartu Monitoring Bimbingan</span>
-            </div>
-            <div class="v113_1123">
-                <span class="v113_1124">Template Laporan PKL</span>
-            </div>
-            <div class="v113_1125">
-                <span class="v118_1243">Formulir Permintaan Dosen Bimbingan</span>
-            </div>
+        <div class="v80_845">
+            <div class="name"></div>
+            <span class="v80_847">Tambah Bimbingan</span>
         </div>
     </div>
 </body>
-
 </html>
