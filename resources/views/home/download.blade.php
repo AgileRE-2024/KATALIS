41d71a59fe1cc72b55af9e6bcfd50381db40724e
eunikewalla <eunikewalllaa@gmail.com>
2024-09-15 18:13:43 +0700

    update

--- resources/views/home/download.blade.php
+++ resources/views/home/download.blade.php
@@ -1,96 +1,138 @@
 <!DOCTYPE html>
-<html lang="en">
-
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Monitoring PKL</title>
     <link href="https://fonts.googleapis.com/css?family=Inter&display=swap" rel="stylesheet" />
     <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet" />
-    <link href="{{ asset('assets/css/main.css') }}" rel="stylesheet" />
+    <link href="./assets/css/donlot.css" rel="stylesheet" />
+    <title>Document</title>
 </head>
-
 <body>
-    <div class="v113_1089">
-        <div class="name"></div>
-        <span class="v113_1116">Download</span>
-        <div class="v123_1694">
-            <div class="v123_1695">
+    <div class="v70_869">
+        <div class="v70_870"></div>
+        <div class="v70_871">
+            <div class="v70_872">
                 <div class="name"></div>
                 <div class="name"></div>
-                <span class="v123_1698">Abi</span>
-                <span class="v123_1699">187345678</span>
+                <span class="v70_875">Eunike Alfrita MW</span>
+                <span class="v70_876">187345678</span>
             </div>
         </div>
-        <div class="v113_1090">
-            <span class="v113_1091">Monitoring PKL</span>
-            <div class="v144_287">
-            <button onclick="location.href='{{ url('profil') }}'">
-                <div class="v113_1092">
-                    
-                    <span class="v113_1094">Profil</span>
-                    
+        <div class="v70_877">
+            <span class="v70_878">Monitoring PKL</span>
+            <div class="v70_1134">
+                <div class="v70_880">
+                    <div class="v70_881">
+                        <a href="profileMahasiswa"><span class="v70_882">Profil</span></a>
+                        <div class="name"></div>
+                    </div>
+                </div>
+                <div class="v70_884">
+                    <div class="v70_885">
+                        <div class="name"></div>
+                        <a href="profileMahasiswa"><span class="v70_882">Informasi PKL</span></a>
+                        <div class="name"></div>
+                    </div>
+                </div>
+                <div class="v70_889">
+                    <div class="v70_890">
+                        <span class="v70_891">Monitoring PKL</span>
+                        <div class="name"></div>
+                    </div>
+                </div>
+                <div class="v70_893">
+                    <div class="v70_894">
+                        <div class="name"></div>
+                        <span class="v70_896">Laporan</span>
+                        <div class="name"></div>
+                    </div>
+                </div>
+                <div class="v70_898">
+                    <div class="name"></div>
+                    <span class="v70_900">Seminar</span>
+                    <div class="name"></div>
                     <div class="name"></div>
                 </div>
-            </button>
-            <button onclick="location.href='{{ url('download') }}'">
-                <div class="v113_1095">
-                    <span class="v113_1097">Download</span>
-                    <div class="name"></div>
+                <div class="v70_903">
+                    <div class="v70_904">
+                        <span class="v70_905">Download</span>
+                        <div class="name"></div>
+                    </div>
                 </div>
-            </button>
-            <button onclick="location.href='{{ url('pengajuan') }}'">
-                <div class="v113_1098">
-                    <div class="name"></div>
-                    <span class="v113_1100">Pengajuan PKL</span>
-                    <div class="name"></div>
-                </div>
-            </button>
-            <button onclick="location.href='{{ url('informasi') }}'">
-                <div class="v113_1101">
-                    <div class="name"></div>
-                    <span class="v113_1103">Informasi PKL</span>
-                    <div class="name"></div>
-                </div>
-            </button>
-            <button onclick="location.href='{{ url('logbook') }}'">
-                <div class="v113_1104">
-                    <span class="v113_1106">Logbook</span>
-                    <div class="name"></div>
-                </div>
-            </button>
-            <button onclick="location.href='{{ url('bimbingan') }}'">
-                <div class="v113_1107">
-                    <span class="v113_1109">Bimbingan</span>
-                    <div class="name"></div>
-                </div>
-            </button>
-            <button onclick="location.href='{{ url('laporan') }}'">
-                <div class="v120_1310">
-                    <div class="name"></div>
-                    <span class="v120_1312">Laporan</span>
-                    <div class="name"></div>
+                <div class="v70_907">
+                    <div class="v70_908">
+                        <div class="name"></div>
+                        <span class="v70_910">Dosen Pembimbing</span>
+                        <div class="v70_911"></div>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="v118_1244">
-            <div class="v113_1117">
-                <span class="v113_1118">Dokumen</span>
+        <div class="v70_912"></div>
+        <div class="v70_913">
+            <div class="v70_914">
+                <div class="name"></div>
+                <div class="name"></div>
             </div>
-            <div class="v113_1119">
-                <span class="v113_1120">Template Surat Pengantar PKL</span>
+        </div>
+        <div class="v70_917">
+            <div class="v70_918">
+                <span class="v70_919">Dokumen</span>
             </div>
-            <div class="v113_1121">
-                <span class="v113_1122">Kartu Monitoring Bimbingan</span>
+            <div class="v70_920">
+                <span class="v70_921">Template Surat Pengantar PKL</span>
+                <div class="v70_922">
+                    <span class="v70_923">Download</span>
+                    <div class="v70_924"></div>
+                </div>
             </div>
-            <div class="v113_1123">
-                <span class="v113_1124">Template Laporan PKL</span>
+            <div class="v70_925">
+                <span class="v70_926">Template Proposal</span>
+                <div class="v70_927">
+                    <span class="v70_928">Download</span>
+                    <div class="v70_929"></div>
+                </div>
             </div>
-            <div class="v113_1125">
-                <span class="v118_1243">Formulir Permintaan Dosen Bimbingan</span>
+            <div class="v70_930">
+                <span class="v70_931">Template Laporan Akhir</span>
+                <div class="v70_932">
+                    <span class="v70_933">Download</span>
+                    <div class="v70_934"></div>
+                </div>
+            </div>
+        </div>
+        <span class="v70_935">Download</span>
+        <div class="v70_936">
+            <div class="v70_937">
+                <div class="name"></div>
+                <div class="name"></div>
+            </div>
+        </div>
+        <div class="v70_940">
+            <div class="v70_941">
+                <span class="v70_942">Logo</span>
+            </div>
+            <div class="v70_943">
+                <span class="v70_944">Logo Unair (Biru)</span>
+                <div class="v70_945">
+                    <span class="v70_946">Download</span>
+                    <div class="v70_947"></div>
+                </div>
+            </div>
+            <div class="v70_948">
+                <span class="v70_949">Lambang Universitas Airlangga</span>
+                <div class="v70_950">
+                    <span class="v70_951">Download</span>
+                    <div class="v70_952"></div>
+                </div>
+            </div>
+            <div class="v70_953">
+                <span class="v70_954">Logo Smart University</span>
+                <div class="v70_955">
+                    <span class="v70_956">Download</span>
+                    <div class="v70_957"></div>
+                </div>
             </div>
         </div>
     </div>
 </body>
-
 </html>
