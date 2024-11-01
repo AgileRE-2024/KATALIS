6d3e24c498ee70bbe7fea264f43cba1f0e27f2db
NabilaErwintria <nzaahiya@gmail.com>
2024-11-01 15:03:39 +0700

    new update

--- resources/views/mahasiswa/infodosbing.blade.php
+++ resources/views/mahasiswa/infodosbing.blade.php
@@ -56,51 +56,47 @@
     <div class="container-scroller">
         <!-- partial:../../partials/_navbar.html -->
         @include('components.navbar')
-        <!-- partial -->
+        
         <div class="container-fluid page-body-wrapper">
-            <!-- partial:../../partials/_settings-panel.html -->
-            <div id="right-sidebar" class="settings-panel">
-                <div class="tab-content" id="setting-content">
-                    <div class="tab-pane fade" id="chats-section" role="tabpanel" aria-labelledby="chats-section">
-                        <div class="d-flex align-items-center justify-content-between border-bottom">
-                            <p class="settings-heading border-top-0 mb-3 pl-3 pt-0 border-bottom-0 pb-0">Friends</p>
-                            <small class="settings-heading border-top-0 mb-3 pt-0 border-bottom-0 pb-0 pr-3 font-weight-normal">See All</small>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- partial -->
-            <!-- partial:../../partials/_sidebar.html -->
             @include('components.sidebarfix')
-            <!-- partial -->
+
             <div class="main-panel">
                 <div class="content-wrapper">
-                    <!-- Begin content here -->
-                    <div class="content">
+                    <!-- Profile Section -->
+                    <div class="profile-card">
+                        <!-- Foto Profil -->
+                        <img src="./assets/images/faces/face26.jpg" alt="Foto Profil" class="profile-photo">
+                        <!-- Informasi Dosen (Nama, Jabatan, NIP) di tengah -->
+                        <div class="profile-info">
+                            <h2>Nabila S.si. M.si</h2>
+                            <h4>Departemen Matematika</h4>
+                            <h4>NIP: 197001011990021001</h4>
+                        </div>
 
-                        <div class="main-content">
-                            <div class="message-container">
-                                <div class="message-text">
-                                    <i class="fas fa-exclamation-triangle"></i>
-                                    <span>Mohon maaf anda belum mempunyai dosen pembimbing</span>
-                                </div>
-                                <a href="formPengajuanDosbing" class="button-link">Ajukan permohonan dosen pembimbing</a>
+                        <!-- Garis Pembatas -->
+                        <div class="divider"></div>
+
+                        <!-- Detail Informasi Dosen -->
+                        <div class="profile-details">
+                            <!-- Kolom Pertama -->
+                            <div class="column">
+                                <p><strong>Nama:</strong> Nabila S.si. M.si</p>
+                                <p><strong>Departemen:</strong> Matematika</p>
+                                <p><strong>NIP:</strong> 197001011990021001</p>
+                                <p><strong>Tanggal Lahir:</strong> 01 Januari 2004</p>
+                            </div>
+
+                            <!-- Kolom Kedua -->
+                            <div class="column">
+                                <p><strong>Alamat Kantor:</strong> Gedung B, Lantai 2, Universitas Airlangga</p>
+                                <p><strong>Email:</strong> nabila@gmail.ac.id</p>
+                                <p><strong>Nomor HP:</strong> 08123456789</p>
+                                <p><strong>Bidang Keahlian:</strong> Sistem Informasi, Data Science</p>
                             </div>
                         </div>
                     </div>
-                    <!-- End content here -->
                 </div>
-                <!-- content-wrapper ends -->
-                <!-- partial:../../partials/_footer.html -->
-                <footer class="footer">
-                    <div class="d-sm-flex justify-content-center justify-content-sm-between">
-                        <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © 2021. Premium <a href="https://www.bootstrapdash.com/" target="_blank">Bootstrap admin template</a> from BootstrapDash. All rights reserved.</span>
-                        <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Hand-crafted & made with <i class="ti-heart text-danger ml-1"></i></span>
-                    </div>
-                </footer>
-                <!-- partial -->
             </div>
-            <!-- main-panel ends -->
         </div>
         <!-- page-body-wrapper ends -->
     </div>
