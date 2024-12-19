1eb23ee43c4f781c59ae7b6dd84a2769c416297a
unknown <go.billy.alexander@gmail.com>
2024-12-19 16:00:27 +0700

    vite

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,12 @@ export default defineConfig({
             input: [
                 'resources/sass/app.scss',
                 'resources/js/app.js',
+                'vendors/feather/feather.css',
+                'resources/css/app.css',
+                'vendors/ti-icons/css/themify-icons.css',
+                'vendors/css/vendor.bundle.base.css',
+                'vendors/datatables.net-bs4/dataTables.bootstrap4.css',
+                'vendors/ti-icons/css/themify-icons.css',
             ],
             refresh: true,
         }),
